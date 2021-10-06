d8bbc141e4f2aee52bb9b568fc1d6d43e1e3fc78
anggunsulastri <treder.maltim.kubar@gmail.com>
2021-10-07 01:52:02 +0800

    first commit

---
+++ validd.js
@@ -0,0 +1,7 @@
+var email = document.forms['form']['email'];
+var password = document.forms['form']['password'];
+
+var email_error = document.getElementById('emai_error');
+var pass_error = document.getElementById('pass_error');
+
+function validated(){}
